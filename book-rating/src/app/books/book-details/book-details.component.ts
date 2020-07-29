f7a3fe93c86c0bce36952246cdf6d625142d3b36
Ferdinand Malcher <ferdinand@malcher.media>
2020-07-29 14:38:40 +0200

    Routing

---
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BookStoreService } from '../shared/book-store.service';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'br-book-details',
+  templateUrl: './book-details.component.html',
+  styleUrls: ['./book-details.component.scss']
+})
+export class BookDetailsComponent implements OnInit {
+
+  book: Book;
+
+  constructor(
+    private route: ActivatedRoute,
+    private bs: BookStoreService) { }
+
+  ngOnInit(): void {
+    // Synchroner Weg
+    // const isbn = this.route.snapshot.paramMap.get('isbn');
+    // console.log(isbn);
+
+    // TODO: Verschachteltes Subscribe vermeiden!
+    this.route.paramMap.subscribe(params => {
+      const isbn = params.get('isbn');
+      console.log(isbn);
+
+      this.bs.getSingle(isbn).subscribe(book => {
+        this.book = book;
+      });
+    });
+  }
+
+}
