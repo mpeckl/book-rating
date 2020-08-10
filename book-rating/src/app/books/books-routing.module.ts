88b671b3021a69225b3ada4b0fd9220c8342cced
DESKTOP-U215CLD\marti <martin@peckl.de>
2020-08-10 17:02:56 +0200

    alte create-Komponente gelöscht

--- book-rating/src/app/books/books-routing.module.ts
+++ book-rating/src/app/books/books-routing.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
-import { CreateBookComponent } from './create-book/create-book.component';
 import { SearchComponent } from './search/search.component';
 import { CreateBookParentComponent } from './create-book-parent/create-book-parent.component';
 import { EditBookComponent } from './edit-book/edit-book.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
-  { path: 'create', component: CreateBookComponent },
-  { path: 'createComponent', component: CreateBookParentComponent},
+  { path: 'create', component: CreateBookParentComponent},
   { path: 'update/:isbn', component: EditBookComponent},
   { path: 'search', component: SearchComponent },
   { path: ':isbn', component: BookDetailsComponent }
--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -5,7 +5,6 @@ import { BooksRoutingModule } from './books-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
-import { CreateBookComponent } from './create-book/create-book.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SearchComponent } from './search/search.component';
 import { BookFormComponent } from './book-form/book-form.component';
@@ -18,7 +17,6 @@ import { EditBookComponent } from './edit-book/edit-book.component';
     DashboardComponent,
     BookComponent,
     BookDetailsComponent,
-    CreateBookComponent,
     SearchComponent,
     BookFormComponent,
     CreateBookParentComponent,
